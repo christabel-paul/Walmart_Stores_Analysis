f4eca0b9bf8686a88d40bdbe25a97f63e944cd09
christabel-paul <59347339+christabel-paul@users.noreply.github.com>
2021-10-15 17:00:37 -0400

    Add files via upload

---
+++ SQLQuery3.sql
@@ -0,0 +1,17 @@
+
+
+--select AVG(Unemployment) as AverageUnemployment
+--From WalmartStoresales
+--group by Store
+
+--average fuel price and average unemployment for stores for total time frame
+select AVG(Fuel_Price) as AverageFuelPrice, AVG(Unemployment) as AverageUnemployment, AVG(Weekly_Sales) as avgStoreSales
+From WalmartStoresales
+group by Store
+
+
+--get number of holiday weeks and see if it affects sales
+select Holiday_Flag as HolidayFlag, count(*) as NumberOfWeeks, AVG(Weekly_Sales) as averageWeeklySales
+From WalmartStoresales
+group by Holiday_Flag
+
